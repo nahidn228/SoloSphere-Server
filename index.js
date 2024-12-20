252e78284459de58b9248bd7e91b28f0ef68b9a5
Nahid Hasan <nahidn228@gmail.com>
2024-12-20 23:25:19 +0600

    implement sort by deadline

--- index.js
+++ index.js
@@ -180,6 +180,9 @@ async function run() {
     app.get("/all-jobs", async (req, res) => {
       const filter = req.query.filter;
       const search = req.query.search;
+      const sort = req.query.sort;
+      let options = {};
+      if (sort) options = { sort: { deadline: sort === "asc" ? 1 : -1 } };
       let query = {
         title: {
           $regex: search,
@@ -190,7 +193,7 @@ async function run() {
 
       console.log(search);
 
-      const result = await jobsCollection.find(query).toArray();
+      const result = await jobsCollection.find(query, options).toArray();
       res.send(result);
     });
 
